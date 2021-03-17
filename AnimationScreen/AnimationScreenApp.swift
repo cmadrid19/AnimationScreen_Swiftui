749a2574832db1fd36716dadfe9d405f643a1e58
Max <maxim.macari.dam@gmail.com>
2021-03-17 01:30:54 +0100

    Initial Commit

---
+++ AnimationScreen/AnimationScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationScreenApp.swift
+//  AnimationScreen
+//
+//  Created by Maxim Macari on 17/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
